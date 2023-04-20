7263b2f5f2ab45c78de5f885e875f489ac3ca2a9
Diego Gómez Moreno <dgomezmdev@gmail.com>
2023-04-21 01:22:50 +0200

    Update

--- src/main/java/app/ChatItemController.java
+++ src/main/java/app/ChatItemController.java
@@ -1,4 +1,18 @@
 package app;
 
-public class ChatItemController {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+
+import java.io.IOException;
+
+public class ChatItemController extends HBox {
+
+    public void initialize() {
+
+    }
+    public void onClick() throws IOException {
+        System.out.println("test");
+    }
 }
--- src/main/java/app/HomeController.java
+++ src/main/java/app/HomeController.java
@@ -3,21 +3,31 @@ package app;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+
 import java.io.IOException;
 
 public class HomeController {
 
     @FXML private BorderPane homeView;
+    @FXML private VBox chatsList;
 
     public void initialize() throws IOException {
 
-/*
         FXMLLoader loader = new FXMLLoader(getClass().getResource("placeholder-view.fxml"));
         homeView.setCenter(loader.load());
- */
+        /*
         FXMLLoader loader = new FXMLLoader(getClass().getResource("chat-view.fxml"));
         homeView.setCenter(loader.load());
+         */
 
+        for (int i = 0; i < 25; i++) {
+            loader = new FXMLLoader(getClass().getResource("chat-item-view.fxml"));
+            chatsList.getChildren().add(loader.load());
+        }
+    }
 
+    public BorderPane getHomeView() {
+        return homeView;
     }
 }
